9a66b47d611b88aecd67be3a24dcf77f4c8543f7
Ivan Bukrieiev <plag.ikar@gmail.com>
2023-11-30 17:45:10 +0100

    initial commit

---
+++ src/main/java/com/kcurryjib/KCurryJibApplication.java
@@ -0,0 +1,13 @@
+package com.kcurryjib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KCurryJibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KCurryJibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcurryjib/KCurryJibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcurryjib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KCurryJibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
