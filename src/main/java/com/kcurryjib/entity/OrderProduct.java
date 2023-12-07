4e5b1b83d4a2c125c937348f7f28e8523f5af763
Ivan Bukrieiev <plag.ikar@gmail.com>
2023-12-07 01:07:11 +0100

    feat(Order/OrderProduct): equals/hasCode/toString

--- src/main/java/com/kcurryjib/entity/OrderProduct.java
+++ src/main/java/com/kcurryjib/entity/OrderProduct.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "order_product")
@@ -39,5 +40,80 @@ public class OrderProduct {
       this.product = product;
       this.quantity = quantity;
       this.total = total;
+   }
+
+   public long getId() {
+      return id;
+   }
+
+   public void setId(long id) {
+      this.id = id;
+   }
+
+   public Order getOrder() {
+      return order;
+   }
+
+   public void setOrder(Order order) {
+      this.order = order;
+   }
+
+   public Product getProduct() {
+      return product;
+   }
+
+   public void setProduct(Product product) {
+      this.product = product;
+   }
+
+   public int getQuantity() {
+      return quantity;
+   }
+
+   public void setQuantity(int quantity) {
+      this.quantity = quantity;
+   }
+
+   public BigDecimal getTotal() {
+      return total;
+   }
+
+   public void setTotal(BigDecimal total) {
+      this.total = total;
+   }
+
+   public LocalDateTime getCratedAt() {
+      return cratedAt;
+   }
+
+   public void setCratedAt(LocalDateTime cratedAt) {
+      this.cratedAt = cratedAt;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      OrderProduct that = (OrderProduct) o;
+      return id == that.id && quantity == that.quantity && Objects.equals(order, that.order) &&
+              Objects.equals(product, that.product) && Objects.equals(total, that.total) &&
+              Objects.equals(cratedAt, that.cratedAt);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(id, order, product, quantity, total, cratedAt);
+   }
+
+   @Override
+   public String toString() {
+      return "OrderProduct{" +
+              "id=" + id +
+              ", order=" + order +
+              ", product=" + product +
+              ", quantity=" + quantity +
+              ", total=" + total +
+              ", cratedAt=" + cratedAt +
+              '}';
    }
 }
