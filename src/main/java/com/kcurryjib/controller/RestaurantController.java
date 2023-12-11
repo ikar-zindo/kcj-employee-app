93a0aedbb823f5e3ccc7cd3bbb9e61e04da39aa9
Ivan Bukrieiev <plag.ikar@gmail.com>
2023-12-11 02:55:02 +0100

    no-fix(Controllers)

---
+++ src/main/java/com/kcurryjib/controller/RestaurantController.java
@@ -0,0 +1,26 @@
+package com.kcurryjib.controller;
+
+import com.kcurryjib.entity.Restaurant;
+import com.kcurryjib.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/restaurant")
+public class RestaurantController {
+
+   @Autowired
+   RestaurantService service;
+
+   @GetMapping
+   public ResponseEntity<List<Restaurant>> getAll() {
+      List<Restaurant> restaurants = service.getAll();
+      return new ResponseEntity<>(restaurants, HttpStatus.OK);
+   }
+}
