477cbe4035ac365951b583c6f2dfa62450fbc982
Ivan Bukrieiev <plag.ikar@gmail.com>
2024-01-09 12:19:58 +0100

    fix(security)

--- src/main/java/com/kcurryjib/entity/Employee.java
+++ src/main/java/com/kcurryjib/entity/Employee.java
@@ -2,13 +2,17 @@ package com.kcurryjib.entity;
 
 import com.kcurryjib.entity.enums.Role;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "employee")
-public class Employee /*implements UserDetails*/ {
+public class Employee implements UserDetails {
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -86,8 +90,29 @@ public class Employee /*implements UserDetails*/ {
       this.email = email;
    }
 
+   @Override
    public String getUsername() {
       return username;
+   }
+
+   @Override
+   public boolean isAccountNonExpired() {
+      return true;
+   }
+
+   @Override
+   public boolean isAccountNonLocked() {
+      return true;
+   }
+
+   @Override
+   public boolean isCredentialsNonExpired() {
+      return true;
+   }
+
+   @Override
+   public boolean isEnabled() {
+      return true;
    }
 
    public void setUsername(String username) {
@@ -102,6 +127,15 @@ public class Employee /*implements UserDetails*/ {
       this.role = role;
    }
 
+   @Override
+   public Collection<? extends GrantedAuthority> getAuthorities() {
+      return AuthorityUtils.createAuthorityList(
+//              String.valueOf(role.getAuthority()));
+              String.valueOf(this.role));
+   }
+
+
+   @Override
    public String getPassword() {
       return password;
    }
--- src/main/java/com/kcurryjib/service/admin/EmployeeService.java
+++ src/main/java/com/kcurryjib/service/admin/EmployeeService.java
@@ -9,6 +9,10 @@ import com.kcurryjib.mapper.admin.EmployeeMapper;
 import com.kcurryjib.repo.EmployeeRepository;
 import com.kcurryjib.repo.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class EmployeeService /*implements UserDetailsService*/ {
+public class EmployeeService implements UserDetailsService {
 
    private EmployeeRepository employeeRepository;
 
@@ -25,14 +29,20 @@ public class EmployeeService /*implements UserDetailsService*/ {
    private RestaurantRepository restaurantRepository;
 
 
+   private BCryptPasswordEncoder encoder;
+
+
    @Autowired
    public EmployeeService(EmployeeRepository employeeRepository,
                           EmployeeMapper employeeMapper,
-                          RestaurantRepository restaurantRepository) {
+                          RestaurantRepository restaurantRepository,
+                          BCryptPasswordEncoder encoder) {
+
 
       this.employeeRepository = employeeRepository;
       this.employeeMapper = employeeMapper;
       this.restaurantRepository = restaurantRepository;
+      this.encoder = encoder;
    }
 
    //READ
@@ -47,5 +57,30 @@ public class EmployeeService /*implements UserDetailsService*/ {
       List<Employee> employees = new ArrayList<>(employeeRepository.findAll());
 
       return employees;
+   }
+
+   @Override
+   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+      UserDetails employee = employeeRepository.findByUsername(username);
+
+      if (employee == null) {
+         throw new UsernameNotFoundException("Employee not found!");
+      }
+
+      return employee;
+   }
+
+   public Employee save(Employee employee) {
+      Employee foundEmployee = (Employee) employeeRepository.findByUsername(employee.getUsername());
+
+      if (foundEmployee != null) {
+         return null;
+      }
+
+      employee.setRole(Role.ROLE_USER);
+      employee.setPassword(encoder.encode(employee.getPassword()));
+      employee.setCreatedAt(LocalDateTime.now());
+
+      return employeeRepository.save(employee);
    }
 }
