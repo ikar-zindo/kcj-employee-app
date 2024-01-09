a0cccc2c693581b92e1ddbc56f59330731e6e02d
Ivan Bukrieiev <plag.ikar@gmail.com>
2024-01-09 16:56:01 +0100

    fix(Security): SecurityController

--- src/main/java/com/kcurryjib/config/SecurityConfig.java
+++ src/main/java/com/kcurryjib/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.kcurryjib.config;
 
-import com.kcurryjib.service.admin.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,7 +8,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -31,11 +28,17 @@ public class SecurityConfig {
       http // configuration of available methods
               .csrf(AbstractHttpConfigurer::disable)
               .sessionManagement(x -> x.sessionCreationPolicy(STATELESS))
+              .logout((logout) -> logout.logoutUrl("/logout").permitAll()
+                      .logoutSuccessUrl("/"))
               .authorizeHttpRequests(
                       x -> x
+                              .requestMatchers(HttpMethod.GET, "/login").permitAll()
                               .requestMatchers(HttpMethod.GET, "/**").permitAll()
-                              .requestMatchers(HttpMethod.GET, "/admin/**").permitAll()
-                              .requestMatchers(HttpMethod.GET, "/employees/**").permitAll()
+                              .requestMatchers(HttpMethod.GET, "/admin/**").hasRole("ADMIN")
+//                              .requestMatchers(HttpMethod.GET, "/login").permitAll()
+//                              .requestMatchers(HttpMethod.GET, "/admin/**").hasAnyRole("ADMIN", "USER")
+//                              .requestMatchers(HttpMethod.GET, "/admin/**").permitAll()
+//                              .requestMatchers(HttpMethod.GET, "/employees/**").permitAll()
 //                              .requestMatchers(HttpMethod.GET, "/employees/all").permitAll()
 //                              .requestMatchers(HttpMethod.GET, "/admin/menu").permitAll()
 //                              .requestMatchers(HttpMethod.GET, "/admin/rest/dashboard").permitAll()
@@ -43,7 +46,10 @@ public class SecurityConfig {
 //                              .requestMatchers(HttpMethod.GET, "/employees/name").hasAnyRole("ADMIN", "USER")
 //                              .requestMatchers(HttpMethod.POST, "/employees/save").hasRole("ADMIN")
                               .anyRequest().authenticated())
-              .httpBasic(Customizer.withDefaults());
+              .formLogin((form) -> form
+                      .loginPage("/login")
+                      .permitAll());
+//              .httpBasic(Customizer.withDefaults());
 
       return http.build();
    }
