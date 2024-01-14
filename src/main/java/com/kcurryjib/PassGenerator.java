6f249763ba200c61a48413d5443b00b6d29c5642
Ivan Bukrieiev <plag.ikar@gmail.com>
2024-01-14 14:12:25 +0100

    style(PassGenerator)

--- src/main/java/com/kcurryjib/PassGenerator.java
+++ src/main/java/com/kcurryjib/PassGenerator.java
@@ -3,8 +3,6 @@ package com.kcurryjib;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PassGenerator {
-
-
    public static void main(String[] args) {
 
       BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
